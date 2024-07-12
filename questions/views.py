864ddfb3ca781d4d3333ff7243bbdeeaf6513664
Marc Cocchio <marccocchio@gmail.com>
2024-07-12 15:59:56 +0900

    Properly sorting chapter-question for index and vocabulary

--- questions/views.py
+++ questions/views.py
@@ -4,8 +4,12 @@ from django.template import loader
 from django.urls import reverse
 from .models import Question, Vocabulary
 
+#for sorting
+from django.db.models import IntegerField, F
+from django.db.models.functions import Cast, Substr
+
 def index(request):
-    question_list = Question.objects.all().order_by('question_code')
+    question_list = sort_chapters_and_questions()
     context = {"question_list": question_list}
     #render() function takes the request object as its first argument, a template name 
     #as its second argument and a dictionary as its optional third argument
@@ -69,10 +73,12 @@ def success(request):
     return render(request, "questions/success.html")
 
 def allvocab(request):
-    vocab_list = Vocabulary.objects.all().order_by('question__question_code')  # Get and sort related vocabulary entries
-    # leverage Django's powerful ORM capabilities to easily navigate and query related models.
-    # question__question_code tells Django to order the Vocabulary instances by the question_code field of the related Question instances.
-    # __ (double underscore) is used to follow the relationship.
+    sorted_questions = sort_chapters_and_questions()
+    vocab_list = Vocabulary.objects.filter(question__in=sorted_questions).order_by(
+        Cast(Substr('question__question_code', 1, 1), IntegerField()),
+        Cast(Substr('question__question_code', 3), IntegerField())
+    )
+
     context = {
         "vocab_list": vocab_list,
     }
@@ -81,3 +87,12 @@ def allvocab(request):
 #step 1: create the view
 def landing_page(request):
     return render(request, 'landing_page.html')
+
+#sorting
+def sort_chapters_and_questions():
+    questions = Question.objects.annotate(
+        chapter_number=Cast(Substr('question_code', 1, 1), IntegerField()),
+        question_number=Cast(Substr('question_code', 3), IntegerField())
+    ).order_by('chapter_number','question_number')
+
+    return questions
